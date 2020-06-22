ee1fd74982c1fc32df68224cb3ff79ea67abbdc9
caroldf07 <carolina.df@outlook.com>
2020-06-22 17:59:14 -0300

    exe 02 - parte 02

--- routes.js
+++ routes.js
@@ -2,8 +2,16 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get("/", (req,res) => {
-    return res.render("layout")
+    return res.render("index")
 })
+
+// routes.get("/instructors", (req,res) => {
+//     return res.render("instructors")
+// })
+
+// routes.get("/students", (req,res) => {
+//     return res.render("students")
+// })
 
 routes.use(function(req, res) {
     res.status(404).render("not-found");
