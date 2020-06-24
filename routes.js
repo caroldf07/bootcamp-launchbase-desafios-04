f6ddd3926f4ce866c823fef52dfd3001ee03ae73
caroldf07 <carolina.df@outlook.com>
2020-06-23 21:45:11 -0300

    exe 03 - validacao

--- routes.js
+++ routes.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./public/instructors/instructors')
 
 routes.get("/", (req,res) => {
+  return res.redirect("instructors")
+})
+
+routes.get("/instructors", (req,res) => {
     return res.render("instructors/index")
 })
 
-// routes.get("/instructors", (req,res) => {
-//     return res.render("instructors")
-// })
+routes.post("/instructors", instructors.post)
+
+routes.get("/instructors/create", (req,res) => {
+    return res.render("instructors/create")
+})
 
 // routes.get("/students", (req,res) => {
 //     return res.render("students")
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const routes = require('./routes')
 const nunjucks = require('nunjucks')
+const { urlencoded } = require('express')
 
 const server = express()
 
-server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }))
 server.use(routes)
+server.use(express.static('public'))
+
+
 
 server.set("view engine", "njk")
 
